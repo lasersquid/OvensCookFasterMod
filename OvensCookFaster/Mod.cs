1c45eef4c9231a2d9e5d5f81b38da01cc1807ed3
lasersquid <levit@DESKTOP-96BFOCF>
2025-05-12 18:48:04 -0600

    Add project files.

---
+++ OvensCookFaster/Mod.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(OvensCookFaster.OvensCookFasterMod), "OvensCookFaster", "1.0.0", "lasersquid", null)]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+namespace OvensCookFaster
+{
+    public class OvensCookFasterMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("OvensCookFaster mod initialized.");
+        }
+    }
+}
---
+++ OvensCookFaster/OvensCookFaster.cs
@@ -0,0 +1,33 @@
+﻿using Il2CppScheduleOne.ObjectScripts;
+using HarmonyLib;
+
+namespace OvensCookFaster
+{
+    [HarmonyPatch(typeof(OvenCookOperation), "GetCookDuration")]
+    public static class OvenCookOperationGetCookDurationPatch
+    {
+        private static int _divider = 4;
+
+        [HarmonyPostfix]
+        public static int Postfix(OvenCookOperation __instance, ref int __result)
+        {
+            return __result / _divider;
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(OvenCookOperation), MethodType.Constructor)]
+    public static class OvenCookOperationCtorPatch
+    {
+        private static int _divider = 4;
+
+        [HarmonyPostfix]
+        public static OvenCookOperation Postfix(ref OvenCookOperation __result)
+        {
+            __result.cookDuration = __result.cookDuration / _divider;
+
+            return __result;
+        }
+    }
+    */
+}
